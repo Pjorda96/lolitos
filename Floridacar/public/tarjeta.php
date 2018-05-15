3da9c8ea752cd55e6c6ec646738d0ba18d483eca
ALUMNADO\dafeca <mark>
2018-05-15 12:58:42 +0200

    base de la tarjeta

---
+++ Floridacar/public/tarjeta.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/tarjeta.css">
+  </head>
+  <body>
+    <div class="tarjeta">
+      <table style="width:100%">
+        <tr>
+          <td>Salida</td>
+          <td align="right">Destino</td>
+        </tr>
+        <tr>
+          <td>Dia</td>
+          <td colspan="4" align="right">Foto</td>
+        </tr>
+        <tr>
+          <td>Hora</td>
+        </tr>
+        <tr>
+          <td>Precio</td>
+        </tr>
+        <tr>
+          <td>Plazas</td>
+        </tr>
+        <tr>
+          <td>Vehiculo</td>
+          <td align="right">Nombre Apellidos</td>
+        </tr>
+      </table>
+
+  </body>
+</html>
