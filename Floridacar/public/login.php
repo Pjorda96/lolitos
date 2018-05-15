1a911af86ac0ef6db307df3716e2477a04359cdd
Marcos <mapepe01@floridauniversitaria.es>
2018-05-15 10:24:01 +0200

    login simple

---
+++ Floridacar/public/login.php
@@ -0,0 +1,18 @@
+<?php
+ ?>
+ <!DOCTYPE html>
+ <html>
+   <head>
+     <meta charset="utf-8">
+     <title>Inicio de Sesión</title>
+   </head>
+   <body>
+     <form class="" action="index.html" method="post">
+       <label for="usuario"><b>Nombre de Usuario: </b></label> <input type="text" name="usuario" id="usuario">
+       <br>
+       <label for="password"><b>Constraseña: </b></label> <input type="text" name="password" id="password">
+       <br>
+       <button type="button" name="button">Entrar</button>
+     </form>
+   </body>
+ </html>
