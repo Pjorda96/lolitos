88fe681d81087e012657027ea12377d1b211af34
ALUMNADO\joalga01 <josebeniparrell96@gmail.com>
2018-05-17 10:34:11 +0200

    Quienes Somos

--- Floridacar/public/views/quienes_somos.php
+++ Floridacar/public/views/quienes_somos.php
@@ -4,11 +4,32 @@
     <meta charset="utf-8">
     <title></title>
     <style media="screen" type="text/css">
-    .quienes{
-      width: 100%;
-      height: 10%;
-      position: absolute;
-      vertical-align:bottom;
+    .texto{
+      font-family: Century Gothic,CenturyGothic,AppleGothic,sans-serif;
+
+      color: #ffffff;
+
+      font-size: 18px;
+
+      font-weight: 400;
+
+      text-align: center;
+
+      background: #889ccf;
+
+      margin: 0 0 25px;
+
+      overflow: hidden;
+
+      padding: 70px;
+
+      border-radius: 35px 0px 35px 0px;
+
+      -moz-border-radius: 35px 0px 35px 0px;
+
+      -webkit-border-radius: 05px 0px 35px 0px;
+
+      border: 2px solid #5878ca;
     }
     .contenedor{
       position: relative;
@@ -16,6 +37,15 @@
       width: 100%;
       height: 60%;
       text-align: center;
+    }
+    .first2{
+      position: relative;
+      width: 20%;
+      height: 60%;
+      padding: 25px;
+      float: left;
+      left:4%;
+      top: 5%;
     }
     .first{
       position: relative;
@@ -33,6 +63,8 @@
       left:5%;
       top: 5%;
     }
+
+
     .third{
       position: relative;
       width: 20%;
@@ -59,6 +91,12 @@
       height: 100%;
       position: absolute;
     }
+    h1 {
+    text-align: center;
+    font-family:Franklin Gothic Medium;
+    font-style: italic;
+    font-weight: 500;
+}
     </style>
   </head>
   <body>
@@ -81,8 +119,14 @@
         a
       </div>
     </div>
-    <div class="quienes" style="background-color: black;">
-      qq
+    <div class="texto">
+      <img src="../images/QSomos.png" width="400">
+<h1>Somos un grupo de estudiantes de primer año del modulo superior de Desarollo de Aplicaciones Web que hemos decidido crear FloridaCar una empresa enfocada al transporte de alumnos al centro de la Florida o de esta al destino que les interese, y asi proporcionar una ayuda économica a los alumnos tanto con vehiculo como sin el.
+Nuestro fin con este programa es ayudar a los alumnos a moverse de un modo economico y de una manera eficiente,en la que principalmente los alumnos sin vehículo no dependan del transporte publico.
+Creemos que seria una buena manera de ahorrar dinero y de crear vinculos entre alumnos de diferentes cursos.</h1>
+
+
+      </div>
     </div>
   </div>
   </body>
