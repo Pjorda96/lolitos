5ee2e1fa1f4dfd96234be21e73c985444e5d52c8
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-05-17 14:46:16 +0200

    modificaciones para que funcione jaja

--- Floridacar/src/models/Usuario.php
+++ Floridacar/src/models/Usuario.php
@@ -80,7 +80,7 @@ class Usuario extends Db
 
               return $resultado;
     }
-    
+
     public function tarjetaCompleta(){
         $tarjetasCompletas="SELECT destino,fecha,precio,salida,plazas,hora,tarjetas.vehiculo,nombre,apellidos,
 FROM tarjetas inner join perxtar on tarjetas.id_tarjeta=perxtar.id_tarjeta
@@ -88,7 +88,6 @@ inner join persona on persona.dni=perxtar.dni where  perxtar.id_tarjeta=";
         $resultado = parent::consultar($tarjetasCompletas);
 
               return $resultado;
-        }
     }
 
     /*public function lista(){
