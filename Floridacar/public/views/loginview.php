eb97aaa655e5c9435d51788e856681b79ee334a7
ALUMNADO\joalga01 <josebeniparrell96@gmail.com>
2018-05-18 12:55:26 +0200

    css login

--- Floridacar/public/views/loginview.php
+++ Floridacar/public/views/loginview.php
@@ -7,14 +7,59 @@ use Car\models\Usuario;
    <head>
      <meta charset="utf-8">
      <title>Inicio de Sesión</title>
+     <style media="screen" type="text/css">
+     body{
+  background-image: url('../images/fondologin.png');
+	background-color: #444;
+
+}
+
+.cuadrado{
+text-align:center;
+  position:absolute;
+  width:310px;
+  height:310px;
+  top:50%;
+  left:45%;
+  margin-top:-100px;
+  margin-left:-100px;
+}
+#container{
+  height: 400px;
+
+}
+
+.color{
+color: black;
+font-size: 150%;
+}
+</style>
    </head>
    <body>
-     <form class="" action="index.html" method="post">
-       <label for="usuario"><b>Nombre de Usuario: </b></label> <input type="text" name="usuario" id="usuario">
-       <br>
-       <label for="password"><b>Constraseña: </b></label> <input type="text" name="password" id="password">
-       <br>
-       <button type="button" name="button">Entrar</button>
+
+      <form class="" action="index.html" method="post">
+
+
+                 <div class="cuadrado"  style="background-color: white;">
+                  <h3 class="color">Introduce tu cuenta</h3>
+                   <form accept-charset="UTF-8" role="form">
+
+                      <input class="texto" placeholder="usuario" name="usuario" type="text">
+                      <br>
+                      <br>
+
+                       <input class="texto" placeholder="password" name="password" type="password" value="">
+                       <br>
+                       <br>
+                       <br>
+
+                     <input class="" type="submit" value="Login">
+
+                     <input type="button"class="button_active" onclick="location.href='../registro.php';"  name="Registrarse" value="Registrarse">
+
+                     </form>
+                 </div>
+
      </form>
    </body>
  </html>
