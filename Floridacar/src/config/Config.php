00dcdbed8e583863b0e5cb96b712204cfc1267a2
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-05-21 11:11:43 +0200

    config

--- Floridacar/src/config/Config.php
+++ Floridacar/src/config/Config.php
@@ -5,8 +5,8 @@ namespace Car\config;
  */
 class Config
 {
-  const CFG_HOST="34.243.177.174";
+  const CFG_HOST="localhost";
   const CFG_USER="root";
-  const CFG_PASS="Ym1B6bbDUfl5";
+  const CFG_PASS="";
   const CFG_DB="bbdd_ip";
 }
