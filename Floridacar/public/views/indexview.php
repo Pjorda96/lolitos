44c9d1f184c3ac18bf780c5061577c2cdfcf7027
ALUMNADO\dafeca <mark>
2018-05-22 10:25:22 +0200

    act

--- Floridacar/public/views/indexview.php
+++ Floridacar/public/views/indexview.php
@@ -55,22 +55,10 @@
     </header>
 
     <main role="main">
-
-      <section class="jumbotron text-center">
-        <div class="container">
-          <h1 class="jumbotron-heading">Album example</h1>
-          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
-          <p>
-            <a href="#" class="btn btn-primary my-2">Main call to action</a>
-            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
-          </p>
-        </div>
-      </section>
-
       <div class="album py-5 bg-light">
         <div class="container">
           <?php foreach ($filasTarjetas as $fila){
-              echo "<a href='tarjeta.php'>";
+              echo "<a href='tarjeta.php?id=".$fila['id_tarjeta']."'>";
               echo "<button type='button' name='button'>";
               echo "<table style='width:200px' border='1px'>";
               echo "<tr>";
