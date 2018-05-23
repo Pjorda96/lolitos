d055c7fd08f243ab668d456fb762b1004a254b5e
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-05-23 09:38:24 +0200

    mod index

--- Floridacar/public/views/indexview.php
+++ Floridacar/public/views/indexview.php
@@ -50,13 +50,9 @@
       </div>
       <div class="navbar navbar-dark bg-dark box-shadow">
         <div class="container d-flex justify-content-between">
-          <a href="#" class="navbar-brand d-flex align-items-center">
+          <a href="index.php" class="navbar-brand d-flex align-items-center">
             <i class="fas fa-car"></i>
 
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/master
             <strong>FloridaCar</strong>
           </a>
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
