ef2b92efdd71b743454a25289216b1dee943e1db
PABLOPC\PabloPortatil <pjorda96@gmail.com>
2018-05-24 10:04:09 +0200

    active directory funciona

---
+++ Floridacar/public/activeDirectory.php
@@ -0,0 +1,27 @@
+<?php
+session_start();
+    $adServer = "ldap://10.2.72.98";
+
+    $ldap = ldap_connect($adServer);
+    //$username = 'admin';
+    //$password = 'Pene1234';
+    $username = $_POST['username'];
+    $password = $_POST['pass'];
+
+    $ldaprdn = 'PABLO0' . "\\" . $username;
+    ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+    ldap_set_option($ldap, LDAP_OPT_REFERRALS, 0);
+    $bind = @ldap_bind($ldap, $ldaprdn, $password);
+    /*if ($bind) {
+    	$msg = "Estás logueado como correctamente como $username";
+    } else {
+        $msg = "Usuario o contraseña incorrectos";
+    }
+    echo $msg;*/
+    if ($bind) {
+        $_SESSION['usuario'] = $_POST['username'];
+        header('Location: index.php');
+    } else {
+        $msg = false;
+    }
+    echo $msg;
--- Floridacar/public/login.php
+++ Floridacar/public/login.php
@@ -2,3 +2,4 @@
 require_once  __DIR__.'/../vendor/autoload.php';
 use Car\models\Usuario;
 include_once("views/loginview.php");
+
